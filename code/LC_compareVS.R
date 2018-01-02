db61f550b59e75556715c9dace3fff92920057d7
Sz-Tim <tim.szewczyk@gmail.com>
2018-01-02 11:31:40 -0500

    added OOS validation

--- code/LC_compareVS.R
+++ code/LC_compareVS.R
@@ -10,38 +10,43 @@ chn.dir <- "lc_small/out/"
 sum.dir <- "lc_small/summaries/"
 mods <- c("Cens", "Clim", "ClimCens", "Topo", 
           "TopoCens", "TopoClim", "TopoClimCens")
-out.type <- c("gelman", "geweke", "loo", "out_all", "out_betas",
-              "out_thin_chains", "rhats_all", "rhats_beta", "summary", "waic")
 
 
 
 # Rhats
-rhat.beta.ls <- map(list.files(sum.dir, pattern="rhat_beta", full.names=TRUE),
-                   ~read.csv(., col.names=c("Parameter", "Rhat")))
-walk(rhat.beta.ls, ~hist(.$Rhat))
-rhat.all.ls <- map(list.files(sum.dir, pattern="rhat_all", full.names=TRUE),
-                    ~read.csv(., col.names=c("Parameter", "Rhat")))
-walk(rhat.all.ls, ~hist(.$Rhat))
+map(list.files(sum.dir, pattern="rhat_beta", full.names=TRUE),
+                   ~read.csv(., col.names=c("Parameter", "Rhat"))) %>%
+  walk(., ~hist(.$Rhat))
+map(list.files(sum.dir, pattern="rhat_all", full.names=TRUE),
+                    ~read.csv(., col.names=c("Parameter", "Rhat"))) %>%
+  walk(., ~hist(.$Rhat))
 
 
 # WAIC
-waic.ls <- map(list.files(sum.dir, pattern="waic", full.names=TRUE),
-               readRDS) 
+waic.ls <- map(list.files(sum.dir, pattern="waic", full.names=TRUE), readRDS) 
 names(waic.ls) <- mods
-map_dfr(waic.ls, ~(.$waic))
+map_dbl(waic.ls, ~(.$waic)) %>% sort
+compare(waic.ls[[1]], waic.ls[[2]], waic.ls[[3]], waic.ls[[4]], 
+        waic.ls[[5]], waic.ls[[6]], waic.ls[[7]])
 
 
 # LOO
-loo.ls <- map(list.files(sum.dir, pattern="loo", full.names=TRUE),
-               readRDS) 
+loo.ls <- map(list.files(sum.dir, pattern="loo", full.names=TRUE), readRDS) 
 names(loo.ls) <- mods
-map_dfr(loo.ls, ~(.$looic))
+map_dbl(loo.ls, ~(.$looic)) %>% sort
+compare(loo.ls[[1]], loo.ls[[2]], loo.ls[[3]], loo.ls[[4]], 
+        loo.ls[[5]], loo.ls[[6]], loo.ls[[7]])
 
+# OOS prediction
+mspe.ls <- map(list.files(sum.dir, pattern="MSPE", full.names=TRUE), readRDS)
+names(mspe.ls) <- mods
+map_dbl(mspe.ls, sqrt) %>% sort
 
 # Gelman
 par(mfrow=c(3,3))
 map(list.files(sum.dir, pattern="gelman", full.names=TRUE), readRDS) %>% 
-  walk2(.x=., .y=mods, ~(hist(x=.$psrf[,2], main=.y, xlim=c(0.95, 2))))
+  walk2(.x=., .y=mods, ~({hist(x=.$psrf[,2], main=.y, xlim=c(0.95, 2)); 
+    abline(v=1.1, lty=3, col="red")}))
 map(list.files(sum.dir, pattern="out_betas", full.names=TRUE), readRDS) %>% 
   walk2(.x=., .y=mods,  ~(gelman.plot(x=.x, ylab=paste(.y, "shrink factor"),
                                       ylim=c(0.95, 1.2))))
