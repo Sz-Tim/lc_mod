284cbd203ae29a3da12cc8a0ed4f7cb747dc6b85
Tim Szewczyk <tim.szewczyk@gmail.com>
2018-05-08 16:49:58 -0600

    code for the cluster

---
+++ code/hpc/3km_full_non.R
@@ -0,0 +1,18 @@
+library(stringr); library(doSNOW)
+
+n.chain <- 3
+n.iter <- 10000
+p.c <- makeCluster(7); registerDoSNOW(p.c)
+
+v <- str_remove(grep("_VS", list.files("data_stan"), inv=T, value=T), ".Rdump")
+foreach(i=seq_along(v)) %dopar% {
+  library(rstan)
+  options(mc.cores=parallel::detectCores()); rstan_options(auto_write=TRUE)
+  mod <- paste0(v[i], "_nonspatial")
+  out <- stan(file="R/nonspatial.stan",
+              data=read_rdump(paste0("data_stan/", v[i], ".Rdump")),
+              pars=c("nu", "Z_", "Z_new_"), include=F,
+              iter=n.iter, chains=n.chain, refresh=50, thin=20,
+              sample_file=paste0("out_stan/", mod, ".csv"))
+}
+stopCluster(p.c)
---
+++ code/hpc/3km_vs_car.R
@@ -0,0 +1,18 @@
+library(stringr); library(doSNOW)
+
+n.chain <- 3
+n.iter <- 10000
+p.c <- makeCluster(7); registerDoSNOW(p.c)
+
+v <- str_remove(list.files("data_stan", "_VS"), ".Rdump")
+foreach(i=seq_along(v)) %dopar% {
+  library(rstan)
+  options(mc.cores=parallel::detectCores()); rstan_options(auto_write=TRUE)
+  mod <- paste0(v[i], "_spatial_vs")
+  out <- stan(file="R/spatial_vs.stan",
+              data=read_rdump(paste0("data_stan/", v[i], ".Rdump")),
+              pars=c("nu", "Z_", "Z_new_"), include=F,
+              iter=n.iter, chains=n.chain, refresh=50, thin=10,
+              sample_file=paste0("out_stan/", mod, ".csv"))
+}
+stopCluster(p.c)
